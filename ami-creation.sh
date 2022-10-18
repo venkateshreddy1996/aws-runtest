e74e38eb6781084d06d2ffc488f9381d98b39b38
Venkatesh <venkateshdevarapati@gmail.com>
2022-10-18 17:43:26 +0530

    add the ec2 files

--- ami-creation.sh
+++ ami-creation.sh
@@ -4,4 +4,5 @@ aws ec2 create-image \
     --instance-id i-06a875c97dbd15750 \
     --name "My server" \
     --description "An AMI for my server" \
-    --tag-specifications file://tags-image.json
+    --output text \
+    --tag-specifications 'ResourceType=image,Tags=[{Key=customKey1,Value=customValue1},{Key=customKey2,Value=customvalue2}]' 'ResourceType=snapshot,Tags=[{Key=customKey1,Value=customValue1},{Key=customKey2,Value=customvalue2}]'
