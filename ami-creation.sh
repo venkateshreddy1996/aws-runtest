108073dae099038e06b08669a88515e1ee552015
Venkatesh <venkateshdevarapati@gmail.com>
2022-10-18 17:38:38 +0530

    add the ec2 files

--- ami-creation.sh
+++ ami-creation.sh
@@ -4,4 +4,4 @@ aws ec2 create-image \
     --instance-id i-06a875c97dbd15750 \
     --name "My server" \
     --description "An AMI for my server" \
-    --tag-specifications file://tags.json
+    --tag-specifications file://tags-image.json
