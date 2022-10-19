c4dc524d045e3de26d441f8ca0ea1b4ed0bde24e
Venkatesh <venkateshdevarapati@gmail.com>
2022-10-19 12:07:27 +0530

    add the ec2 files

--- autosclaing-launch-configuration.sh
+++ autosclaing-launch-configuration.sh
@@ -5,6 +5,7 @@ IMAGE=ami-064dfa47f06f71079
 Instance_type=t2.micro
 security_groups=sg-091613d2180615619
 Key=devkey-rjt
+region=ap-south-1
 IAM_instance_profile="Arn=arn:aws:iam::925034260775:instance-profile/Allow-admin-fromec2"
 
 aws autoscaling create-launch-configuration \
@@ -14,3 +15,4 @@ aws autoscaling create-launch-configuration \
     --security-groups "${security_groups}" \
     --iam-instance-profile "${IAM_instance_profile}" \
     --key-name ${Key}
+    --region ${region}
