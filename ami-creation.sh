5f68d0bb1068a70995228224b2097ba6d8d86de3
Venkatesh <venkateshdevarapati@gmail.com>
2022-10-19 11:37:24 +0530

    add the ec2 files

--- ami-creation.sh
+++ ami-creation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 aws ec2 create-image \
-    --instance-id i-06a875c97dbd15750 \
-    --name "My server" \
+    --instance-id i-0059a565187319d70 \
+    --name "JenkinsBackupAMi" \
     --description "An AMI for my server" \
     --output text \
     --region ap-south-1 \
