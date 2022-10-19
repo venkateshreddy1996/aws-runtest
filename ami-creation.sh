bbed0646e1b651ddb51b086a15ce88b5e509dbd8
Venkatesh <venkateshdevarapati@gmail.com>
2022-10-19 12:05:04 +0530

    add the ec2 files

--- ami-creation.sh
+++ ami-creation.sh
@@ -4,6 +4,6 @@ aws ec2 create-image \
     --instance-id i-07832c93498a3bcb6 \
     --name "WebserverAMI" \
     --description "Taking AMI backup for webserver" \
-    --output text \
+    --output json \
     --region ap-south-1 \
     --no-reboot
--- autosclaing-launch-configuration.sh
+++ autosclaing-launch-configuration.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+
+NAME=webserver-lc
+IMAGE=ami-064dfa47f06f71079
+Instance_type=t2.micro
+security_groups=sg-091613d2180615619
+Key=devkey-rjt
+IAM_instance_profile="Arn=arn:aws:iam::925034260775:instance-profile/Allow-admin-fromec2"
+
+aws autoscaling create-launch-configuration \
+    --launch-configuration-name ${NAME} \
+    --image-id ${IMAGE} \
+    --instance-type ${Instance_type} \
+    --security-groups "${security_groups}" \
+    --iam-instance-profile "${IAM_instance_profile}" \
+    --key-name ${Key}
